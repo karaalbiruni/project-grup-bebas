1d1a7973aee2b9ec2433fc77dabaf6e0396ae140
Hendry Setiawan <setiawanhendry227@gmail.com>
2022-08-16 13:40:22 +0700

    perubahan_1.1
    nambahin faktorial, satuin fungsi minimal sama maksimum sama benerin struktur code keseluruhan

--- calculator.cpp
+++ calculator.cpp
@@ -4,271 +4,309 @@
 //bagi ✔
 //rata2 ✔
 //factorial ✔
-//pangkat
-//akar
+//pangkat✔
+//akar 
 //cari min ✔
 //cari max ✔
 //median
 //modus ✔
-
+#include <stdbool.h>
 #include <stdio.h>
 
-int main()
-{
-	printf("Enter mau melakukan operasi apa\n");
-	printf("Ketik 1 untuk tambah\n");
-	printf("Ketik 2 untuk kurang\n");
-	printf("Ketik 3 untuk kali\n");
-	printf("Ketik 4 untuk bagi\n");
-	printf("Ketik 5 untuk rata2\n");
-	printf("Ketik 6 untuk pangkat\n");
-	printf("Ketik 7 untuk akar\n");
-	printf("Ketik 8 untuk factorial\n");
-	printf("Ketik 9 untuk minimum\n");
-	printf("Ketik 10 untuk maximum\n");
-	printf("Ketik 11 untuk median\n");
-	printf("Ketik 12 untuk modus\n");
-	int operasi;
-	scanf ("%d", &operasi);
-	
-	if (operasi == 1)
-	{
-		printf("Mau berapa nomer\n");
-		int x;
-		scanf("%d", &x);
-		int sum = 0;
-		printf("Masukin nomer2 nya\n");
-		
-		for (int i = 1; i <= x; i++)
-		{
-			int y[i];
-			scanf("%d", &y[i]);
-			
-			if (i == 1)
-			{
-				sum = y[i];
-			}
-			else 
-			{
-				sum += y[i];	
-			}
-			
-		}
-		printf("the result is %d", sum);
+//faktorial
+int factorial(int number){
+	if(number ==1){
+		return 1;
+	}else{
+		return number * factorial(number-1) ;
 	}
-	else if (operasi == 2)
-	{
-		printf("Mau berapa nomer\n");
-		int x;
-		scanf("%d", &x);
-		int sum = 0;
-		printf("Masukin nomer2 nya\n");
-		
-		for (int i = 1; i <= x; i++)
-		{
-			int y[i];
-			scanf("%d", &y[i]);
+}
+
+//pangkat
+int minMax(int arrayLen, bool min){
+	int array[arrayLen];
+	int res = 0;
+	for (int i = 0; i < arrayLen; i++){
+
+			int array[i];
+			scanf("%d", &array[i]);
 			
-			if (i == 1)
-			{
-				sum = y[i];
-			}
-			else 
-			{
-				sum -= y[i];	
-			}
-			
-		}
-		printf("the result is %d", sum);
-	}
-	else if (operasi == 3)
-	{
-		printf("Mau berapa nomer\n");
-		int x;
-		scanf("%d", &x);
-		int sum = 0;
-		printf("Masukin nomer2 nya\n");
-		
-		for (int i = 1; i <= x; i++)
-		{
-			int y[i];
-			scanf("%d", &y[i]);
-			
-			if (i == 1)
-			{
-				sum = y[i];
-			}
-			else 
-			{
-				sum *= y[i];	
-			}
-			
-		}
-		printf("the result is %d", sum);
-	}
-	
-	else if (operasi == 4)
-	{
-	printf("Masukkan pembilang dan penyebut!\n");
-	float x, y;
-	scanf("%f %f", &x, &y);
-	
-	
-	
-	printf("the result is %f\n", x/y);
-	}
-	
-	else if (operasi == 5)
-	{
-		printf("Mau berapa nomer\n");
-		int x;
-		scanf("%d", &x);
-		float sum = 0;
-		printf("Masukin nomer2 nya\n");
-		
-		for (int i = 1; i <= x; i++)
-		{
-			float y[i];
-			scanf("%f", &y[i]);
-			
-			if (i == 1)
-			{
-				sum += y[i];
-			}
-			else 
-			{
-				sum += y[i];	
-			}
-			
-		}
-		printf("the result is %f\n", sum/x);
-	}
-	
-	else if (operasi == 6)
-	{
-		puts("Masukkan suatu bilangan dan pastikan bilangan tersebut >= 0");
-		int a;
-		scanf("%d", &a);
-		if (a < 2) {
-		printf("%d\n", 1);
-	} else {
-		int hasil = 1;
-		for (int i = a; i >= 1; i--) {
-		hasil *= i;
-	}
-	printf("%d\n", hasil);
-	}
-	
-	}
-	
-	else if (operasi == 9)
-	{
-		printf("Mau berapa nomer\n");
-		int t;
-		scanf("%d", &t);
-		long long int sum = 0;
-		printf("Masukin nomer2 nya\n");
-		
-		for (int i = 0; i < t; i++)
-		{
-			int arr[i];
-			scanf("%d", &arr[i]);
-			
-			if (i == 0)
-			{
-				sum = arr[i];
-			}
-			else if (sum >= arr[i])
-			{
-				sum = arr[i];
-			}
-		}
-		
-		printf("the result is %d", sum);
-		
-	}
-	
-	else if (operasi == 10)
-	{
-		printf("Mau berapa nomer\n");
-		int t;
-		scanf("%d", &t);
-		long long int sum = 0;
-		printf("Masukin nomer2 nya\n");
-		
-		for (int i = 0; i < t; i++)
-		{
-			int arr[i];
-			scanf("%d", &arr[i]);
-			
-			if (i == 0)
-			{
-				sum = arr[i];
-			}
-			else if (sum <= arr[i])
-			{
-				sum = arr[i];
-			}
-		}
-		
-		printf("the result is %d", sum);
-		
-	}
-	
-	else if (operasi == 12)
-	{
-		puts("mau berapa nomer");
-		int n; // testcase
-		scanf("%d", &n);
-		int arr[n];
-		puts("masukin nomer2nya");
-		
-		for (int i = 0; i < n; i++)
-		{
-			scanf("%d", &arr[i]);
-		}
-		
-		int maxValue = 0, maxCount = 0, count = 0;
-		
-		for (int i = 0; i < n; i++)
-		{
-			count = 0;
-			
-			for (int j = 0; j < n; j++)
-			{
-				if (arr[i] == arr[j])
-				{
-					count++;
+			if(min == true){
+				if (i == 0){
+					res = array[i];
+				}
+
+				if (res >= array[i]){
+					res = array[i];
+				}
+			}else{
+				if(res<= array[i]){
+					res = array[i];
 				}
 			}
+			
+		}
+	return res;
+}
+
+
+int main(){	
+	int operasi;
+
+	while(true){
+		do{
+		printf("\nEnter mau melakukan operasi apa\n");
+		printf("Ketik 1 untuk tambah\n");
+		printf("Ketik 2 untuk kurang\n");
+		printf("Ketik 3 untuk kali\n");
+		printf("Ketik 4 untuk bagi\n");
+		printf("Ketik 5 untuk rata2\n");
+		printf("Ketik 6 untuk pangkat\n");
+		printf("Ketik 7 untuk akar\n");
+		printf("Ketik 8 untuk factorial\n");
+		printf("Ketik 9 untuk minimum\n");
+		printf("Ketik 10 untuk maximum\n");
+		printf("Ketik 11 untuk median\n");
+		printf("Ketik 12 untuk modus\n");
+		printf("Ketik 13 untuk kembali ke menu utama\n");
+
+		printf("input:");
+		scanf ("%d", &operasi);
+		getchar();
+		}while( operasi < 1 || operasi > 13);
+	
+	
+		if (operasi == 1)
+		{
+			printf("Mau berapa nomer\n");
+			int x;
+			scanf("%d", &x);
+			int sum = 0;
+			printf("Masukin nomer2 nya\n");
+			
+			for (int i = 1; i <= x; i++)
+			{
+				int y[i];
+				scanf("%d", &y[i]);
+				
+				if (i == 1)
+				{
+					sum = y[i];
+				}
+				else 
+				{
+					sum += y[i];	
+				}
+				
+			}
+			printf("the result is %d", sum);
+		}
+		else if (operasi == 2)
+		{
+			printf("Mau berapa nomer\n");
+			int x;
+			scanf("%d", &x);
+			int sum = 0;
+			printf("Masukin nomer2 nya\n");
+			
+			for (int i = 1; i <= x; i++)
+			{
+				int y[i];
+				scanf("%d", &y[i]);
+				
+				if (i == 1)
+				{
+					sum = y[i];
+				}
+				else 
+				{
+					sum -= y[i];	
+				}
+				
+			}
+			printf("the result is %d", sum);
+		}
+		else if (operasi == 3)
+		{
+			printf("Mau berapa nomer\n");
+			int x;
+			scanf("%d", &x);
+			int sum = 0;
+			printf("Masukin nomer2 nya\n");
+			
+			for (int i = 1; i <= x; i++)
+			{
+				int y[i];
+				scanf("%d", &y[i]);
+				
+				if (i == 1)
+				{
+					sum = y[i];
+				}
+				else 
+				{
+					sum *= y[i];	
+				}
+				
+			}
+			printf("the result is %d", sum);
 		}
 		
-		if (count == n)
+		else if (operasi == 4)
 		{
-			printf("There is no mode\n");
+		printf("Masukkan pembilang dan penyebut!\n");
+		float x, y;
+		scanf("%f %f", &x, &y);
+		
+		
+		
+		printf("the result is %f\n", x/y);
 		}
-		else
+		
+		else if (operasi == 5)
 		{
+			printf("Mau berapa nomer\n");
+			int x;
+			scanf("%d", &x);
+			float sum = 0;
+			printf("Masukin nomer2 nya\n");
+			
+			for (int i = 1; i <= x; i++)
+			{
+				float y[i];
+				scanf("%f", &y[i]);
+				
+				if (i == 1)
+				{
+					sum += y[i];
+				}
+				else 
+				{
+					sum += y[i];	
+				}
+				
+			}
+			printf("the result is %f\n", sum/x);
+		}
+		
+		else if (operasi == 6)
+		{
+			puts("Masukkan suatu bilangan dan pastikan bilangan tersebut >= 0");
+			int a;
+			scanf("%d", &a);
+			if (a < 2) {
+				printf("%d\n", 1);
+			} else {
+				int hasil = 1;
+				for (int i = a; i >= 1; i--) {
+				hasil *= i;
+			}
+			printf("%d\n", hasil);
+		}
+		}
+	
+		else if (operasi == 7)
+		{
+			int base, exp;
+			puts("masukin base");
+			scanf("%d", &base);
+			puts("masukin exponent");
+			scanf("%d", &exp);
+			int power;
+			power = 1;
+			
+			for (int i = 1; i <= exp; i++)
+			{
+				power *= base;
+			}
+			
+			printf("hasilnya adalah %d", power);
+		}
+
+		else if(operasi == 8){
+			printf("Under construction\n");
+		}
+		
+		
+		else if (operasi == 9)
+		{
+			printf("Mau berapa nomer\n");
+			int arrayLen;
+			scanf("%d", &arrayLen);
+
+			printf("Masukin nomer2 nya\n");
+			int array_res = minMax(arrayLen,true);
+			
+			printf("the result is %d", array_res);
+			
+		}
+		
+		else if (operasi == 10)
+		{
+			printf("Mau berapa nomer\n");
+			int arrayLen;
+			scanf("%d", &arrayLen);
+
+			printf("Masukin nomer2 nya\n");
+			int array_res = minMax(arrayLen,false);
+			
+			printf("the result is %d", array_res);
+			
+		}
+
+		else if(operasi == 11){
+			printf("Under construction\n");
+		}
+		
+		else if (operasi == 12)
+		{
+			puts("mau berapa nomer");
+			int n; // testcase
+			scanf("%d", &n);
+			int arr[n];
+			puts("masukin nomer2nya");
+			
 			for (int i = 0; i < n; i++)
 			{
-				if (count > maxCount)
+				scanf("%d", &arr[i]);
+			}
+			
+			int maxValue = 0, maxCount = 0, count = 0;
+			
+			for (int i = 0; i < n; i++)
+			{
+				count = 0;
+				
+				for (int j = 0; j < n; j++)
 				{
-					maxCount = count;
-					maxValue = arr[i];
+					if (arr[i] == arr[j])
+					{
+						count++;
+					}
 				}
 			}
 			
-			printf("Your mode is %d\n", maxValue);
+			if (count == n)
+			{
+				printf("There is no mode\n");
+			}
+			else
+			{
+				for (int i = 0; i < n; i++)
+				{
+					if (count > maxCount)
+					{
+						maxCount = count;
+						maxValue = arr[i];
+					}
+				}
+				
+				printf("Your mode is %d\n", maxValue);
+			}
+		}else if (operasi == 13){
+			break;
 		}
 	}
-	
-	
-	
-	
-	/*
-	1. tambah
-	2. kurang
-	*/
-	
 	return 0;
+
 }
