e2a77866617b09573908a46f2366a1570c4a4fcd
Hendry Setiawan <setiawanhendry227@gmail.com>
2022-08-24 19:02:10 +0700

    Update Calculator
    1. menambahkan fitur median dan merge sort

--- calculator.cpp
+++ calculator.cpp
@@ -227,7 +227,84 @@ int modus(int n, int arr[])
 	return maxValue;
 }
 
-int main(){	
+int sorted(int arr[]){
+	for(int i = 1; arr[i] ; i++){
+		if(arr[i] < arr[i-1]){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
+
+double median(int arr[],int length){
+
+	switch (length){
+	case 1:
+		return arr[0];
+		break;
+	
+	default:
+		int middle = length/2;
+		if(length % 2 != 0){	
+			return arr[middle];
+		}else{
+			int midLeft = middle - 1;
+			int midRight = middle + 1;
+			double avg = (double)(arr[midLeft] + arr[midRight]) /2;
+			return avg; 
+		}
+		break;
+	}
+
+}
+
+//melakukan merge sort sebelum menemukan median
+void merge(int arr[],int start , int mid , int end){
+	int leftLength = mid - start + 1; 
+	int rightLength = end - mid;
+	
+	int tempLeft[leftLength];
+	int tempRight[rightLength];
+	
+	int leftStep, rightStep, arrStep;
+	for(leftStep = 0; leftStep <= leftLength - 1; leftStep++){
+		tempLeft[leftStep] = arr[start + leftStep];
+	}
+	
+	for(rightStep = 0; rightStep <= rightLength - 1; rightStep++){
+		tempRight[rightStep] = arr[mid + rightStep + 1];
+	}
+	
+	for(rightStep = 0, leftStep = 0, arrStep = start ; arrStep  <= end ; arrStep++){
+		if((rightStep >= rightLength) || (tempLeft[leftStep] <= tempRight[rightStep])){
+			if(leftStep < leftLength){
+				arr[arrStep] = tempLeft[leftStep];
+				leftStep++;
+			}
+		}else{
+			arr[arrStep] = tempRight[rightStep];
+			rightStep++;
+		}
+	}
+	
+}
+
+void mergeSort(int arr[], int start, int end){
+	if(start < end){
+		int mid = start + (end - start) / 2;
+		mergeSort(arr,start,mid);
+		mergeSort(arr,mid+1,end);
+		
+		merge(arr,start,mid,end);
+	}
+	
+	return;
+}
+
+
+void calculator(){	
 	int operasi;
 
 	//perulangan while dijalankan jika kondisi kalkulator true 
@@ -235,31 +312,31 @@ int main(){
 
 		//menu kalkulator , berhenti jika angka yang diimput adalah 1 - 13
 		do{
+				
+			puts("   _____      _            _       _             ");
+			puts("  / ____|    | |          | |     | |            ");
+			puts(" | |     __ _| | ___ _   _| | __ _| |_ ___  _ __ ");
+			puts(" | |    / _` | |/ __| | | | |/ _` | __/ _ \\| '__|");
+			puts(" | |___| (_| | | (__| |_| | | (_| | || (_) | |   ");
+			printf("  \\_____\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \n\n\n");	
 			
-		puts("   _____      _            _       _             ");
-		puts("  / ____|    | |          | |     | |            ");
-		puts(" | |     __ _| | ___ _   _| | __ _| |_ ___  _ __ ");
-		puts(" | |    / _` | |/ __| | | | |/ _` | __/ _ \\| '__|");
-		puts(" | |___| (_| | | (__| |_| | | (_| | || (_) | |   ");
-		printf("  \\_____\\__,_|_|\\___|\\__,_|_|\\__,_|\\__\\___/|_|   \n\n\n");	
-		
-		printf("Ketik angka 1 - 13 untuk: \n\n");
-		printf("1.  Penjumlahan\n");
-		printf("2.  Pengurangan\n");
-		printf("3.  Perkalian\n");
-		printf("4.  Pembagian\n");
-		printf("5.  Rata-rata\n");
-		printf("6.  Factorial\n");
-		printf("7.  Pangkat\n");
-		printf("8.  Akar\n");
-		printf("9.  Minimum\n");
-		printf("10. Maximum\n");
-		printf("11. Median\n");
-		printf("12. Modus\n");
-		printf("13. Kembali ke menu utama\n\n");
-		printf("Input: ");
-
-		scanf ("%d", &operasi);
+			printf("Ketik angka 1 - 13 untuk: \n\n");
+			printf("1.  Penjumlahan\n");
+			printf("2.  Pengurangan\n");
+			printf("3.  Perkalian\n");
+			printf("4.  Pembagian\n");
+			printf("5.  Rata-rata\n");
+			printf("6.  Factorial\n");
+			printf("7.  Pangkat\n");
+			printf("8.  Akar\n");
+			printf("9.  Minimum\n");
+			printf("10. Maximum\n");
+			printf("11. Median\n");
+			printf("12. Modus\n");
+			printf("13. Kembali ke menu utama\n\n");
+			printf("Input: ");
+	
+			scanf ("%d", &operasi);
 		}while( operasi < 1 || operasi > 13);
 	
 		//pertambahan
@@ -391,7 +468,27 @@ int main(){
 
         //median
 		else if(operasi == 11){
-			printf("Under construction\n");
+			
+			puts("mau berapa nomor");
+			int n; 
+			scanf("%d", &n);
+			int arr[n];
+			puts("masukin nomornya");
+			
+			for (int i = 0; i < n; i++)
+			{
+				scanf("%d", &arr[i]);
+			}
+			
+			int sortCond = sorted(arr);
+			
+			if(sortCond != 1){
+				mergeSort(arr,0,n-1);
+			}
+			
+
+			printf("The result is %.2llf \n\n", median(arr,n));
+			
 		}
 		
 		//modus
@@ -413,10 +510,31 @@ int main(){
 			
 		}
 		
-		else if (operasi == 13){
+		else{
 			break;
 		}
+		
+		
+		char pilihan;
+		
+		do{
+			getchar();
+			printf("Apakah anda ingin membuka kalkator lagi?(Y/T) : ");
+			scanf("%c", &pilihan);
+		}while((pilihan != 'T') && (pilihan != 'Y'));
+		
+		
+		if(pilihan == 'T'){
+			break; // ganti function submenu
+		}else{
+			NULL; //ganti function calculator
+		}
+	
 	}
-	return 0;
 
+}
+
+int main(){
+	calculator();
+	return 0;
 }
